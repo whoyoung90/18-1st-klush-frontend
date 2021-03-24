855f7248a71c7b8d3db7f02715944aad45d8e34c
JAEWOOK JUNG <jaewookjung@JAEWOOKui-MacBookPro.local>
2021-03-24 15:27:28 +0900

    Refactor: 리뷰 반영 수정 완료

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class ProductDetail extends Component {
   render() {
-    return <div>hello this page is for id: {this.props.match.params.id}</div>;
+    return <div>id: {this.props.match.params.id}</div>;
   }
 }
 
