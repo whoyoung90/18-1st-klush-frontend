0e660af75fa16c665aa1e81eddad79937ceb38ce
wooyoungkim <whoyoung90@naver.com>
2021-03-25 15:04:23 +0900

    [18기 김우영]Add: nav1.js 리팩토링까지 완료

--- src/Components/Nav/DropUserMenu.js
+++ src/Components/Nav/DropUserMenu.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./DropUserMenu.scss";
 
+const NAV_USER_LIST = [
+  { path: "/login", value: "로그인" },
+  { path: "/signup", value: "회원가입" },
+  { path: "#", value: "스카우트" },
+  { path: "#", value: "고객센터" },
+];
+
 class DropUserMenu extends Component {
   render() {
-    const NAV_USER_LIST = [
-      { path: "/login", value: "로그인" },
-      { path: "/signup", value: "회원가입" },
-      { path: "#", value: "스카우트" },
-      { path: "#", value: "고객센터" },
-    ];
-
     return (
       <ul className="dropUserMenu">
         {NAV_USER_LIST.map((obj, idx) => {
