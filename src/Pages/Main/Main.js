fcd5ccf38fafb286eb3b88f03edfef1dbb6bf658
wooyoungkim <whoyoung90@naver.com>
2021-03-26 17:32:06 +0900

    [18기 김우영]Add: nav2.js 1차 프로젝트 종료

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -15,7 +15,7 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    fetch("http://10.58.5.223:8000/product?is_new=True")
+    fetch("http://54.180.150.98:8000/product?is_new=True")
       .then(res => res.json())
       .then(res => this.setState({ mainList: res.product_list_data }));
   }
@@ -41,9 +41,14 @@ class Main extends Component {
                 {mainList.map((el, idx) => {
                   return (
                     <Link to="/main" key={idx} className="myItemList">
-                      <img className="myImg" src={el.images} alt="newLabel" />
+                      <img
+                        className="myImg"
+                        src={el.image_url}
+                        alt="newLabel"
+                      />
+                      <img src={el.is_new} />
                       <div className="imgTitle">{el.name}</div>
-                      <div className="imgHash">{el.product_labels}</div>
+                      <div className="imgHash">{el.product_label}</div>
                       <div className="cost">
                         ￦{Math.floor(el.price).toLocaleString()}
                       </div>
